84bffcd248c805f1dbc21d0d6de1a563305f1da5
tarunon <croissant9603@gmail.com>
2017-04-22 01:04:59 +0900

    setup project and dependencies

---
+++ mstdn/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  mstdn
+//
+//  Created by tarunon on 2017/04/22.
+//  Copyright © 2017年 tarunon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
