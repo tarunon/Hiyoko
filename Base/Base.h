0c21a99eaa9417c1b8bbb6d7025f0cb4dd3831b4
tarunon <croissant9603@gmail.com>
2017-04-22 11:25:41 +0900

    update gitignore

---
+++ Base/Base.h
@@ -0,0 +1,19 @@
+//
+//  Base.h
+//  Base
+//
+//  Created by tarunon on 2017/04/22.
+//  Copyright © 2017年 tarunon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Base.
+FOUNDATION_EXPORT double BaseVersionNumber;
+
+//! Project version string for Base.
+FOUNDATION_EXPORT const unsigned char BaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Base/PublicHeader.h>
+
+
---
+++ Base/Sources/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  mstdn
+//
+//  Created by tarunon on 2017/04/22.
+//  Copyright © 2017年 tarunon. All rights reserved.
+//
+
+import Foundation
