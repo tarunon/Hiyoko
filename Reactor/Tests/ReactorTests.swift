9b865708f43eed0d14335aba2ae742134e32a834
tarunon <croissant9603@gmail.com>
2017-05-11 00:28:41 +0900

    add minimum test code.

--- Reactor/Tests/ReactorTests.swift
+++ Reactor/Tests/ReactorTests.swift
@@ -8,29 +8,99 @@
 
 import XCTest
 @testable import Reactor
+import RxSwift
+import RxCocoa
+import RxTest
+
+struct AnyReactor<Action, State, Result>: Reactor {
+    let _process: (Observable<Action>) throws -> Process<State, Result>
+    func process(action: Observable<Action>) throws -> Process<State, Result> {
+        return try _process(action)
+    }
+}
+
+struct AnyView<Action, State>: View {
+    let _present: (Observable<State>) -> Present<Action>
+    
+    func present(state: Observable<State>) -> Present<Action> {
+        return _present(state)
+    }
+}
+
+struct DummyError: Error {
+    static let instance = DummyError()
+}
 
 class ReactorTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testSimplePresent() {
+        let ts = TestScheduler(initialClock: 0)
+        let emurator = PublishSubject<Int>()
+        let simulator = PublishSubject<Int>()
+        let reactor = AnyReactor<Never, Int, Never> { _ in Process(state: emurator, result: Observable.never()) }
+        let view = AnyView<Never, Int> { state in return Present(action: Observable.never(), bind: state.bind(to: simulator)) }
+        
+        let d = ts.createColdObservable(
+            [
+                .init(time: 150, value: .next(1)),
+                .init(time: 250, value: .next(10)),
+                .init(time: 300, value: .next(20)),
+                .init(time: 350, value: .error(DummyError.instance))
+            ]
+        ).bind(to: simulator)
+        
+        let x = ts.start {
+            Observable
+                .merge(
+                    simulator,
+                    Result(view: view, reactor: reactor).asObservable().flatMap { $0.result }.flatMap { _ in Observable.empty() }
+            )
         }
+        
+        XCTAssertEqual(
+            x.events,
+            [
+                .init(time: 250, value: .next(10)),
+                .init(time: 300, value: .next(20)),
+                .init(time: 350, value: .error(DummyError.instance)),
+            ]
+        )
+        
+        XCTAssertFalse(emurator.hasObservers)
+        
+        d.dispose()
     }
     
+    func testSimpleResult() {
+        let ts = TestScheduler(initialClock: 0)
+        let simulator = PublishSubject<Int>()
+        let reactor = AnyReactor<Int, Never, Int> { action in Process(state: Observable.never(), result: action) }
+        let view = AnyView<Int, Never> { _ in return Present(action: simulator) }
+        
+        let d = ts.createColdObservable(
+            [
+                .init(time: 150, value: .next(1)),
+                .init(time: 250, value: .next(10)),
+                .init(time: 300, value: .next(20)),
+                .init(time: 350, value: .error(DummyError.instance))
+            ]
+            ).bind(to: simulator)
+        
+        let x = ts.start {
+            Result(view: view, reactor: reactor).asObservable().flatMap { $0.result }
+        }
+        
+        XCTAssertEqual(
+            x.events,
+            [
+                .init(time: 250, value: .next(10)),
+                .init(time: 300, value: .next(20)),
+                .init(time: 350, value: .error(DummyError.instance)),
+            ]
+        )
+        
+        XCTAssertFalse(simulator.hasObservers)
+        
+        d.dispose()
+    }
 }
